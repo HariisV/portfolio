b6a6ccf8234a16e93e44d16a99be475620ae816b
Haris Wahyudi <hariswh9@gmail.com>
2021-12-19 18:33:37 +0700

    Fix Portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,13 +21,76 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
+    name: 'Tickez',
+    description:
+      'Ticks is an application that is used to book a Movies. built using React, here you can select all movies & select available seats',
+    stack: ['React', 'Redux', 'Bootstrap', 'Express', 'Redis', 'Axios'],
+    sourceCode: 'https://github.com',
+    url: 'https://raw.githubusercontent.com/mesindo07/Portfolio-Image/main/tickez.png',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'xWallet',
+    description:
+      'xWallet is a digital wallet application that allows us to transfer money for free without any fees. also automatic topup',
+    stack: ['Next', 'Redux', 'Bootstrap', 'Redis', 'Express', 'Midtrans'],
+    sourceCode: 'https://github.com',
+    url: 'https://raw.githubusercontent.com/mesindo07/Portfolio-Image/main/wallet.png',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Hire App',
+    description:
+      'HireApp is an application for job seekers or looking for workers. Or even you can contact workers for a project',
+    stack: ['React', 'Redux', 'Redis', 'Bootstrap', 'Express'],
+    sourceCode: 'https://github.com',
+    url: 'https://raw.githubusercontent.com/mesindo07/Portfolio-Image/main/hire.png',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Coffe Shop',
+    description:
+      'Website for buying coffee online. using midtrans as a payment gateway. Admin can manage purchases and view monthly data.',
+    stack: ['React', 'Redux', 'Redis', 'Bootstrap', 'Express', 'Midtrans'],
+    sourceCode: 'https://github.com',
+    url: 'https://raw.githubusercontent.com/mesindo07/Portfolio-Image/main/coffe.png',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'DMA Dropship',
+    description:
+      'A website for buying and selling between admins and users. As well as managing, storing purchase and expenditure data.',
+    stack: ['Laravel', 'Bootstrap', 'Jquery', 'MySql'],
+    // sourceCode: 'https://github.com',
+    livePreview: 'https://dmadropship.com/',
+    url: 'https://raw.githubusercontent.com/mesindo07/Portfolio-Image/main/dma.png',
+  },
+  {
     name: 'Eka Danta LC',
     description:
       'Tryout online website, used for students who study there, every month more than 150 visitors access it at the same time',
     stack: ['Laravel', 'Bootstrap', 'Jquery', 'Ajax'],
-    sourceCode: 'https://github.com',
-    url: 'https://raw.githubusercontent.com/HariisV/portfolio/main/public/image/asd.png?token=ALLV2MNF4O5LPZYANWAH4UDBX32U4',
-    livePreview: 'https://github.com',
+    // sourceCode: 'https://github.com',
+    livePreview: 'https://ekadantalc.com/',
+    url: 'https://raw.githubusercontent.com/mesindo07/Portfolio-Image/main/ekadanta.png',
+  },
+  {
+    name: 'PPDB SMKN 5',
+    description:
+      'New Student Admission Website. has several features such as registering new students, verifying data, to taking online tests.',
+    stack: ['Laravel', 'Bootstrap', 'MySql'],
+    sourceCode: 'https://github.com/HariisV',
+    livePreview: 'https://github.com/HariisV',
+    url: 'https://raw.githubusercontent.com/mesindo07/Portfolio-Image/main/ppdb.png',
+  },
+  {
+    name: 'Meduza Shop',
+    description:
+      'A simple online store website that uses laravel as a backend. This is my first freelance project and learn more about Laravel',
+    stack: ['Laravel', 'Bootstrap', 'MySql'],
+    sourceCode: 'https://github.com/HariisV/Meduza-Shop',
+    url: 'https://raw.githubusercontent.com/mesindo07/Portfolio-Image/main/meduza.png',
+    // livePreview: 'https://github.com',
   },
 ]
 
